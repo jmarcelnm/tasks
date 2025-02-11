9360d42a9d67fe9d542bea304e9e60565e7f020f
jmarcelnm <jmarcelnm@outlook.com>
2025-02-11 18:39:27 +0100

    refactor(auth): protecting the routes

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,13 @@ use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')
-    ->group(
-        function () {
-            Route::apiResource('tasks', TaskController::class);
-        }
-    );
+Route::middleware('auth:sanctum')
+    ->prefix('v1')
+    ->group(function () {
+        Route::apiResource('tasks', TaskController::class);
+    });
 
-Route::get(
-    '/user',
-    function (Request $request) {
+Route::middleware('auth:sanctum')
+    ->get('/user', function (Request $request) {
         return $request->user();
-    }
-)->middleware('auth:sanctum');
+    });

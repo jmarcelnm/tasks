2c053f7a727a523a04539d9eae681af0bf4690d5
jmarcelnm <jmarcelnm@outlook.com>
2025-02-11 16:27:15 +0100

    feat(auth): adding the login route

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided email is not registered.']
+            ]);
+        } elseif (!Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'password' => ['The provided password is incorrect.']
+            ]);
+        }
+    }
+}

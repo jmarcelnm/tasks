a3c77ec0d7cd8b6d876388caf1f02b7a45527645
jmarcelnm <jmarcelnm@outlook.com>
2025-02-11 09:27:53 +0100

    fix: removing default value in the updated_at key

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -20,7 +20,7 @@ class TaskResource extends JsonResource
             'description' => $this->description,
             'completed' => (bool) $this->completed,
             'created_at' => date_format($this->created_at, 'Y-m-d H:i:s'),
-            'updated_at' => date_format($this->updated_at, 'Y-m-d H:i:s')
+            'updated_at' => null
         ];
     }
 }

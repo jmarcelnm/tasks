5343725cddb52f85b14ee5b4e1626e87486523d1
jmarcelnm <jmarcelnm@outlook.com>
2025-02-11 18:56:12 +0100

    refactor(auth): using sanctum for the API token authentication

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     * 
+     * @param LoginRequest $request
+     */
+    public function __invoke(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided email is not registered.']
+            ]);
+        } elseif (!Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'password' => ['The provided password is incorrect.']
+            ]);
+        }
+
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken('auth_token')->plainTextToken
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,22 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\LogoutController;
+use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')
-    ->prefix('v1')
-    ->group(function () {
-        Route::apiResource('tasks', TaskController::class);
-    });
+Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
+    Route::apiResource('tasks', TaskController::class);
+});
 
-Route::middleware('auth:sanctum')
-    ->get('/user', function (Request $request) {
-        return $request->user();
-    });
+Route::prefix('auth')->group(function () {
+    Route::post('/login', LoginController::class)->middleware('guest');
+    Route::post('/logout', LogoutController::class)->middleware('auth');
+    Route::post('/register', RegisterController::class)->middleware('guest');
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
